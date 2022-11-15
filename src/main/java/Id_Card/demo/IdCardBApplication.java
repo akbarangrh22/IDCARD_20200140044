b13cd941f1d0567225bb94e4a27700c2ecbd756b
akbar <anugrahakbar522@gmmail.com>
2022-11-15 22:22:40 +0700

    IDCARD

---
+++ src/main/java/Id_Card/demo/IdCardBApplication.java
@@ -0,0 +1,13 @@
+package Id_Card.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCardBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCardBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Id_Card/demo/IdCardBApplicationTests.java
@@ -0,0 +1,13 @@
+package Id_Card.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
